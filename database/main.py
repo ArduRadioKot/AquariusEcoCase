a6b809b8d04d4d99d59b6f4dccc9dd191d6d8e3d
olesia-kuzina <olesya20102012@yandex.ru>
2025-12-11 03:12:05 +0300

    upd

--- database/main.py
+++ database/main.py
@@ -1,13 +1,32 @@
-from flask import Flask, request
-from . import db_sessyon
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret_key'
+import sqlite3
+import csv
 
 
-def main():
-    db_sessyon.global_init("database/measurments.db")
-    app.run()
+class DB:
+    def __init__(self):
+        self.connection = sqlite3.connect('mea.sqlite')
+        self.cursor = self.connection.cursor()
 
-if __name__ == '__main__':
-    main()
+    def get(self, dat: str):
+        ind_id = self.cursor.execute("""SELECT id FROM indicators WHERE indicator = ?""", (dat,)).fetchone()
+
+        if ind_id is None:
+            return None
+
+        results = self.cursor.execute("""SELECT id, meaning FROM measurements WHERE indicator = ?""",
+                                      (ind_id[0],)).fetchall()
+
+        with open("ans.csv", 'w', encoding='utf-8', newline='') as f:
+            cs = csv.writer(f)
+            cs.writerow(['id', 'meaning'])
+            cs.writerows(results)
+
+        return 'ans.csv'
+
+    def nem_m(self, id_ind: int, id_dev: int, mea: str):
+        self.cursor.execute("""INSERT INTO measurements (meaning, indicator, device) VALUES (?, ?, ?)""",
+                            (mea, id_ind, id_dev))
+        self.connection.commit()
+
+    def close(self):
+        self.connection.close()
